710a46fa51d26b054c2adc8906038e37afa3dd38
pulkit-khanna-yc <pulkit.khanna.work@gmail.com>
2024-08-28 05:56:43 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scavange',
+    version='0.0.1',
+    description='Util functions',
+    author='Sri Senthil Balaji J',
+    author_email='srisenthil.balajij.sg@gretail.com',
+    packages=find_packages(include=['utils*'])
+)
